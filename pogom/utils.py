8947fac923b795293c8dc1dfa9d24fbe109a4676
mmerlin6 <mmerlin@elegantformal.net>
2016-07-29 15:30:40 -0700

    Reduce scan delay

--- pogom/utils.py
+++ pogom/utils.py
@@ -43,7 +43,7 @@ def get_args():
                         default=12)
     parser.add_argument('-sd', '--scan-delay',
                         help='Time delay between requests in scan threads',
-                        type=float, default=5)
+                        type=float, default=1)
     parser.add_argument('-td', '--thread-delay',
                         help='Time delay between each scan thread loop',
                         type=float, default=5)
